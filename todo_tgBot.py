a8dee674a3faba29bea18daf94da12c270bb959c
Andrea <andrearubiolo12@gmail.com>
2019-03-25 21:12:02 +0100

    Implemented all commands

--- todo_tgBot.py
+++ todo_tgBot.py
@@ -6,8 +6,9 @@ from telegram import ChatAction
 hello_msg = "Welcome in AmI Task List 237031 bot!\nHere's a list of commands you can use:\n" \
             "/showTasks - show all the existing tasks in alphabetic order\n" \
             "/newTask - insert a new task\n" \
-            "/removeTask: remove a task (by typing its content, exactly)\n" \
-            "/removeAllTasks: remove all the existing tasks that contain a provided string\n\n" \
+            "/removeTask - remove a task (by typing its content, exactly)\n" \
+            "/removeAllTasks - remove all the existing tasks that contain a provided string\n" \
+            "/help - read again this message\n\n" \
             "All the commands are executed on the 'task_list.txt' file"
 
 # getting the to do list
@@ -39,31 +40,71 @@ def show_task(bot, update):
 
 def new_task(bot, update, args):
     task = " ".join(args)
-    todo.append(task)
 
-    content = open(file_name, "a")
-    content.write(task)
-    content.write('\n')
+    if task != "":
+        todo.append(task)
+        content = open(file_name, "a")
+        content.write(task)
+        content.write('\n')
+        content.close()
+
+        update.message.reply_text("'" + task + "' insert in the list.")
+    else:
+        update.message.reply_text("You must write a task to be added in the list.")
+
+
+def remove_task(bot, update, args):
+    task = " ".join(args)
+    removed = False
+
+    content = open(file_name, "w")
+    for t in todo:
+        if t != task:  # task not to be deleted
+            content.write(task)
+            content.write('\n')
+        else:
+            todo.remove(task)
+            update.message.reply_text("'" + task + "' removed")
+            removed = True
+
+    if not removed:
+        update.message.reply_text("Can't find '" + task + "' in the list")
     content.close()
 
 
-def remove_task(bot, update):
-    update.message.reply_text("Remove")
+def remove_all(bot, update, args):
+    task = " ".join(args)
+    cont = 0  # count of deleted tasks
+    remove_list = []
 
+    content = open(file_name, "w")
+    for t in todo:
 
-def remove_all(bot, update):
-    update.message.reply_text("Remove")
+        if task not in t:  # task not to be deleted
+            content.write(t)
+            content.write('\n')
+        else:
+            remove_list.append(t)
+            cont = cont + 1
+    content.close()
+
+    for t in remove_list:
+        if t in todo:
+            todo.remove(t)
+
+    update.message.reply_text(str(cont) + " task(s) containing '" + task + "' removed\n")
 
 
 def main():
-    updater = Updater(token='TOKEN')
+    updater = Updater(token='TOKEN')  # insert your token instead of TOKEN
 
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", start))
     dp.add_handler(CommandHandler("showTasks", show_task))
     dp.add_handler(CommandHandler("newTask", new_task, pass_args=True))
-    dp.add_handler(CommandHandler("removeTask", remove_task))
-    dp.add_handler(CommandHandler("removeAllTasks", remove_all))
+    dp.add_handler(CommandHandler("removeTask", remove_task, pass_args=True))
+    dp.add_handler(CommandHandler("removeAllTasks", remove_all, pass_args=True))
     dp.add_handler(MessageHandler(Filters.text, echo))
 
     updater.start_polling()
